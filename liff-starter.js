70cb2e7187047e964cb5dddae1d5dd44e1834bdf
AlvinZheng <642893776@qq.com>
2019-01-23 17:12:02 +0800

    Update liff-starter.js
    buy added

--- liff-starter.js
+++ liff-starter.js
@@ -28,13 +28,15 @@ function initializeApp(data) {
     document.getElementById('sendmessagebutton').addEventListener('click', function () {
         liff.sendMessages([{
             type: 'text',
+            text: 'hello'
+        },{
+            type: 'text',
             text: 'buy'
         }]).then(function () {
-            window.alert("Message sent successfully");
+            window.alert("now we will send you videos");
         }).catch(function (error) {
             window.alert("Error sending message: " + error);
         });
-        liff.closeWindow();
     });
 
     //get profile call
